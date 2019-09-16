2ea75e38281033350bf763bd3195ee25a9e2d250
Shymaxtic <shymaxtic@gmail.com>
2019-09-16 08:36:35 +0700

    open/close file

--- USBDriver/shymaxtic_device.c
+++ USBDriver/shymaxtic_device.c
@@ -7,24 +7,72 @@
 
 #define D_DEVICE_NAME       "Shymaxtic USB"
 
+
  struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = shymaxtic_device_open,
-    .release = shymaxtic_device_close
+    .release = shymaxtic_device_close,
+    .read = shymaxtic_device_read,
+    .write = shymaxtic_device_write
 };
 
-int shymaxtic_device_open(struct inode* inode, struct file* filep) {
-    // TODO
+struct shymaxtic_device_t *shymaxtic_dev = NULL;
+
+static int shymaxtic_device_open(struct inode* inode, struct file* filep) {
+    int ret = 0;
+    // Allocate device file
+    struct shymaxtic_device_file_t* shymaxtic_file = NULL;
+    shymaxtic_file = (struct shymaxtic_device_file_t*)kzalloc(sizeof(struct shymaxtic_device_file_t), GFP_KERNEL);
+    if (shymaxtic_file == NULL) {
+        printk(KERN_ERR "Failed to allocate device file\n");
+        return -ENOMEM;
+    }
+    shymaxtic_file->dev = shymaxtic_dev;
+    filep->private_data = shymaxtic_file;
+    if (shymaxtic_dev->udev == NULL) {
+        printk(KERN_WARNING "Usb device not present\n");
+        return -ENOMEM;
+    }
+    // Do task
+    ret = usb_get_descriptor(shymaxtic_dev->udev, USB_DT_STRING, 0, shymaxtic_dev->name, 128);
+    if (ret < 0) {
+        return ret;
+    }
+    printk(KERN_INFO "%s", shymaxtic_dev->name);
+    printk(KERN_INFO "Opened file");
     return 0;
 }
 
-int shymaxtic_device_close(struct inode* inode, struct file* filep) {
-    // TODO
+static int shymaxtic_device_close(struct inode* inode, struct file* filep) {
+    // Deallocate device file
+    int ret = 0;
+    struct shymaxtic_device_file_t* shymaxtic_file = NULL;
+    shymaxtic_file = (struct shymaxtic_device_file_t*)filep->private_data;
+    if (shymaxtic_file == NULL) {
+        printk(KERN_WARNING "Device file not allocated yet\n");
+        return -ENOMEM;
+    }
+    kfree(shymaxtic_file);
+    printk(KERN_INFO "Closed file");
     return 0;
 }
+
+static ssize_t shymaxtic_device_read(struct file *f, char __user *buf, size_t len, loff_t *off) {
+    // TODO
+    printk(KERN_INFO "Read file\n");
+    return 0;
+}
+
+static ssize_t shymaxtic_device_write(struct file *f, const char __user *buf, size_t len, loff_t *off) {
+    // TODO
+    printk(KERN_INFO "Write file\n");
+    return len;
+}
+
 
 int shymaxtic_device_init(struct shymaxtic_device_t** gdevp) {
     int ret = 0;
+    struct device* dev_ret = NULL;
     struct shymaxtic_device_t* gdev = NULL;
     // Allocate memory in kernel for our device.
     *gdevp = kzalloc(sizeof(struct shymaxtic_device_t), GFP_KERNEL);
@@ -43,21 +91,22 @@ int shymaxtic_device_init(struct shymaxtic_device_t** gdevp) {
 
     // Add to /sys/class
     gdev->cl = class_create(THIS_MODULE, "shymaxticdrv");
-    if (gdev->cl == NULL) {
+    if (IS_ERR(gdev->cl)) {
         unregister_chrdev_region(gdev->dev_num, 1);
         return PTR_ERR(gdev->cl);
     }
 
     // Add to /dev
-    if (device_create(gdev->cl, NULL, gdev->dev_num, NULL, "shymaxticdevice") == NULL) {
+    dev_ret = device_create(gdev->cl, NULL, gdev->dev_num, NULL, "shymaxticdevice");
+    if (IS_ERR(dev_ret)) {
         class_destroy(gdev->cl);
         unregister_chrdev_region(gdev->dev_num, 1);
-        return PTR_ERR(NULL);
+        return PTR_ERR(dev_ret);
     }
 
     // Allocate cdev
     gdev->cdev = cdev_alloc();
-    if (gdev->cdev == NULL) {
+    if (IS_ERR(gdev->cdev)) {
         printk(KERN_ERR "Failed to allocate cdev\n");
         return PTR_ERR(gdev->cdev);
     }
--- USBDriver/shymaxtic_device.h
+++ USBDriver/shymaxtic_device.h
@@ -7,7 +7,7 @@
 #include <linux/kernel.h>
 #include <linux/usb.h>
 
-// My data struct .
+// My device .
 struct shymaxtic_device_t {
     struct usb_device       *udev;
     struct cdev             *cdev; 
@@ -18,11 +18,20 @@ struct shymaxtic_device_t {
     struct class            *cl; 
 };
 
+// My file
+struct shymaxtic_device_file_t {
+    struct shymaxtic_device_t* dev;    
+};
+
+
 // File operation callbacks.
-int shymaxtic_device_open(struct inode* inode, struct file* filep);
-int shymaxtic_device_close(struct inode* inode, struct file* filep);
+static int shymaxtic_device_open(struct inode* inode, struct file* filep);
+static int shymaxtic_device_close(struct inode* inode, struct file* filep);
+static ssize_t shymaxtic_device_read(struct file *f, char __user *buf, size_t len, loff_t *off);
+static ssize_t shymaxtic_device_write(struct file *f, const char __user *buf, size_t len, loff_t *off);
 
 extern struct file_operations fops;
+extern struct shymaxtic_device_t *shymaxtic_dev;
 
 int shymaxtic_device_init(struct shymaxtic_device_t** gdev);
 void shymaxtic_device_deinit(struct shymaxtic_device_t* gdev);
