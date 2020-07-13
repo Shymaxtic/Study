846c495a5eecc53a97834b323e03e532229a9a2f
shymaxtic <shymaxtic@gmail.com>
2020-07-13 19:20:22 +0700

    ex

---
+++ python_package_example/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='python_package_example',
+      version='0.1',
+      license='MIT',
+      author='Shymaxtic',
+      author_email='shymaxtic@gmail.com',
+      description='package example',
+      packages=find_packages('.'))
