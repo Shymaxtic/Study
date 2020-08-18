a570ab9a45cfa4ea4a20697d0b4ed91b94676467
Shymaxtic <shymaxtic@gmail.com>
2020-08-17 20:47:48 -0700

    tcpdump example

---
+++ Linux/tcpdump_example/client.py
@@ -0,0 +1,14 @@
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 5000        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    input('Any key to send')
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+    print('Received', repr(data))
+
+    input('Any key to exit')
