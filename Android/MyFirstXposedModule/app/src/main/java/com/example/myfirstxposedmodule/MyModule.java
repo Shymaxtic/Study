b7bcb571308eca0e3625e2b508f5501c7a5286d4
shymaxtic <shymaxtic@gmail.com>
2020-08-29 20:33:43 +0700

    1. example expose module

---
+++ Android/MyFirstXposedModule/app/src/main/java/com/example/myfirstxposedmodule/MyModule.java
@@ -0,0 +1,13 @@
+package com.example.myfirstxposedmodule;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.XposedBridge;
+
+public class MyModule implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
+}
