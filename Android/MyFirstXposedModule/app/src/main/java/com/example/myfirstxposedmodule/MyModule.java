e5b6ff1344a6dd3858f86a8dfaeaa6859a488c1b
shymaxtic <shymaxtic@gmail.com>
2020-08-29 21:09:03 +0700

    1. change color time

--- Android/MyFirstXposedModule/app/src/main/java/com/example/myfirstxposedmodule/MyModule.java
+++ Android/MyFirstXposedModule/app/src/main/java/com/example/myfirstxposedmodule/MyModule.java
@@ -1,6 +1,11 @@
 package com.example.myfirstxposedmodule;
 
+import android.graphics.Color;
+import android.widget.TextView;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import de.robv.android.xposed.XposedBridge;
 
@@ -9,5 +14,15 @@ public class MyModule implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         XposedBridge.log("Loaded app: " + lpparam.packageName);
+        XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock",
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        TextView tv = (TextView) param.thisObject;
+                        String text = tv.getText().toString();
+                        tv.setText(text + " :)");
+                        tv.setTextColor(Color.RED);
+                    }
+                });
     }
 }
